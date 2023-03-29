eb79d3f71cbfc3ae575eade40c355f5517e41dd9
epsibaranis <104891770+epsibaranis@users.noreply.github.com>
2023-03-30 02:11:32 +0530

    First commit

---
+++ Control_Statements/Biggest_Of_three_numbers.py
@@ -0,0 +1,12 @@
+# biggest of three integerno's'
+a=int(input('a=?'))
+b=int(input('b=?'))
+c=int(input('c=?'))
+if a>b:
+    if a>c:
+     print("biggest of three integerno\'s",a)
+else:
+    if b>c:
+     print("biggest of three integerno\'s",b)
+    else:
+     print("biggest of three integerno\'s",c)

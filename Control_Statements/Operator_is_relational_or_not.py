55f730abb0c9dee57d60674f9c932b4fed47464e
epsibaranis <104891770+epsibaranis@users.noreply.github.com>
2023-03-30 02:43:34 +0530

    First commit

---
+++ Control_Statements/Operator_is_relational_or_not.py
@@ -0,0 +1,6 @@
+# Operator is an Relational Operator or not
+a=str(input('a=?'))
+if a=='>' or a=='<' or a=='=' or a=='<=' or a=='>=' or a=='!=':
+    print('Operator is Relational Operator')
+else:
+    print('Operator is Not Relational Operator')

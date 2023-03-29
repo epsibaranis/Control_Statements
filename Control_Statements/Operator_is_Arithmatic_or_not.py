d0bc47220d486c5858851d868c2fa3a77594816b
epsibaranis <104891770+epsibaranis@users.noreply.github.com>
2023-03-30 02:37:31 +0530

    First commit

---
+++ Control_Statements/Operator_is_Arithmatic_or_not.py
@@ -0,0 +1,6 @@
+# Operator is an arithmatic or not
+a=str(input('a=?'))
+if a=='+' or a=='-' or a=='*' or a=='/' or a=='%':
+    print('Operator is Arithmatic Operator')
+else:
+    print('Operator is Not Arithmatic Operator')

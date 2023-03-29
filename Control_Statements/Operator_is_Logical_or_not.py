827b2bf4bd93e8a402c7ba395c8e6f54f617c85f
epsibaranis <104891770+epsibaranis@users.noreply.github.com>
2023-03-30 02:45:05 +0530

    First commit

---
+++ Control_Statements/Operator_is_Logical_or_not.py
@@ -0,0 +1,6 @@
+# Operator is an Logical Operator or not
+a=str(input('a=?'))
+if a=='and' or a=='or' or a=='not':
+    print(' Operator is Logical Operator')
+else:
+    print('Operator is Not Logical Operator')
